edd1c7f95372f1fbd4264a4bd4d394431c5790be
Hayat6488 <abulhayat619@gmail.com>
2022-10-25 19:16:54 +0600

    cors permission added

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express')
-const app = express()
+const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000
+
+app.use(cors());
 
 const subjects = require('./data/subjects.json');
 
 app.get('/', (req, res) => {
     res.send('server is on!!!');
+})
+
+app.get('/subjects', (req, res) => {
+    res.send(subjects);
 })
 
 app.listen(port, ()=>{
